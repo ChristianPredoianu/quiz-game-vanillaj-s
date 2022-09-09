f6fd8fad694d3b39dc415fd19fbe9cef101f84d6
Christian Predoianu <christianpredoianu@yahoo.com>
2022-09-09 15:18:09 +0200

    added getQuestions

--- src/js/openTrivia.js
+++ src/js/openTrivia.js
@@ -1,14 +1,27 @@
+import { displayFetchError } from './ui';
+
 export async function getCategories() {
   const url = 'https://opentdb.com/api_category.php';
 
   try {
     const response = await fetch(url);
-    const categories = await response.json();
-
-    return categories;
+    const data = await response.json();
+    console.log(data);
+    return data;
   } catch (error) {
-    const errorParagraph = document.getElementById('fetch-error-msg');
-    errorParagraph.style.display = 'block';
-    errorParagraph.innerText = error;
+    displayFetchError(error);
+  }
+}
+
+export async function getQuestions(gameOptions) {
+  const url = `https://opentdb.com/api.php?amount=${gameOptions.amountOfQuestions}&category=${gameOptions.category}&difficulty=${gameOptions.difficulty}&type=${gameOptions.questionsOptions}&encode=base64`;
+
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    displayFetchError(error);
   }
 }
