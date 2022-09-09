e7b9119f6a9509a7031102e00693130e1dcbaf75
Christian Predoianu <christianpredoianu@yahoo.com>
2022-09-09 15:18:21 +0200

    work

--- src/js/ui.js
+++ src/js/ui.js
@@ -1,20 +1,17 @@
-import { getCategories } from './openTrivia';
-import { getUserSelectedCategory } from './main';
+import { selectedCategoryCard, currentQuestion } from './main';
 
-export function initGameUi() {
-  document.getElementById('game-section-container').style.display = 'block';
-  removeInitialGameUi();
-
-  getCategories().then((data) => {
-    createCategoryCards(data);
-    displayLoadingSpinner('none');
-    displayCallToActions();
-  });
-
-  displayLoadingSpinner('block');
+export function removeInitialGameUi() {
+  document.getElementById('initial-game-ui').style.display = 'none';
+  document.getElementById('main').style.backgroundImage = 'none';
 }
 
-function createCategoryCards(data) {
+export function displayFetchError(error) {
+  const errorParagraph = document.getElementById('fetch-error-msg');
+  errorParagraph.style.display = 'block';
+  errorParagraph.innerText = error;
+}
+
+export function createCategoryCards(data) {
   const categoriesContainer = document.getElementById('categories-container');
 
   const categories = data.trivia_categories;
@@ -22,39 +19,72 @@ function createCategoryCards(data) {
   categories.forEach((category) => {
     const categoryCard = document.createElement('div');
     categoryCard.className = 'category-card';
+    categoryCard.id = category.id;
     categoryCard.innerText = category.name;
     categoriesContainer.appendChild(categoryCard);
   });
-  addEventListenerToCategoryCards();
+
+  selectedCategoryCard();
 }
 
-export function addEventListenerToCategoryCards() {
-  const categoryCards = document.querySelectorAll('.category-card');
-
-  categoryCards.forEach((categoryCard) => {
-    categoryCard.addEventListener('click', function () {
-      getUserSelectedCategory(this.innerText);
-
-      categoryCards.forEach((categoryCard) => {
-        categoryCard.classList.remove('selected-category-card');
-      });
-
-      this.classList.add('selected-category-card');
-    });
-  });
+export function removeSelectedCategoryClass(categoryCard) {
+  categoryCard.classList.remove('selected-category-card');
 }
 
-function removeInitialGameUi() {
-  document.getElementById('initial-game-ui').style.display = 'none';
-  document.getElementById('main').style.backgroundImage = 'none';
-}
-
-function displayLoadingSpinner(display) {
+export function displayLoadingSpinner(display) {
   const loadingSpinner = document.getElementById('loading-spinner');
   loadingSpinner.style.display = display;
 }
 
-function displayCallToActions() {
+export function displayCallToActions() {
   document.getElementById('game-options').style.display = 'flex';
   document.getElementById('start-game-btn').style.display = 'block';
+}
+
+export function displayOptionsError(display, error = '') {
+  optionsError = document.getElementById('options-error');
+  optionsError.style.display = display;
+  optionsError.innerText = error;
+}
+
+export function pulseCategoryContainer() {
+  document
+    .getElementById('categories-container')
+    .classList.add('pulse-categories-container');
+}
+
+export function removePulseCategoryContainer() {
+  document
+    .getElementById('categories-container')
+    .classList.remove('pulse-categories-container');
+}
+
+export function removeGameOptionsUi() {
+  document.getElementById('category-heading').style.display = 'none';
+  document.getElementById('categories-container').style.display = 'none';
+  document.getElementById('game-options').style.display = 'none';
+  document.getElementById('start-game-btn').style.display = 'none';
+}
+
+export function createQuestion(data, currentQuestion) {
+  console.log(data.results);
+  const gameSectionContainer = document.getElementById(
+    'game-section-container'
+  );
+
+  const questionDiv = document.createElement('div');
+  questionDiv.className = 'question-div';
+  gameSectionContainer.appendChild(questionDiv);
+
+  const questionParagraph = document.createElement('p');
+  questionParagraph.className = 'question-div__paragraph';
+  questionParagraph.innerText = atob(data.results[currentQuestion].question);
+  questionDiv.appendChild(questionParagraph);
+
+  const questionHeading = document.createElement('h3');
+  questionHeading.className = 'question-div__heading';
+  questionHeading.innerText = `Question ${currentQuestion + 1} of ${
+    data.results.length
+  }`;
+  questionDiv.insertBefore(questionHeading, questionParagraph);
 }
