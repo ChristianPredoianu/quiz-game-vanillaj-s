63557fb2dede7418d1202c99aa66c886a445d858
Christian Predoianu <christianpredoianu@yahoo.com>
2022-09-13 16:47:45 +0200

    added session token + code refactoring

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,24 @@
 import * as ui from './ui';
-import { getCategories, getQuestions } from './openTrivia';
+import { fetchSessionToken, useApi } from './openTrivia';
 
 let selectedGameOptions = {};
 let isGamePlaying = false;
-export let currentQuestion = 0;
+let currentQuestion = 0;
+let countdownTime = 5;
+let countdownTimer;
 
 document.getElementById('continue-btn').addEventListener('click', () => {
   initGameUi();
 });
 
 function initGameUi() {
+  const categoryUrl = 'https://opentdb.com/api_category.php';
+
   document.getElementById('game-section-container').style.display = 'block';
+
   ui.removeInitialGameUi();
 
-  getCategories().then((data) => {
+  useApi(categoryUrl).then((data) => {
     ui.createCategoryCards(data);
     ui.displayLoadingSpinner('none');
     ui.displayCallToActions();
@@ -28,7 +33,7 @@ export function selectedCategoryCard() {
   categoryCards.forEach((categoryCard) => {
     categoryCard.addEventListener('click', function () {
       selectedGameOptions.category = this.id;
-
+      console.log(selectedGameOptions);
       categoryCards.forEach((categoryCard) => {
         ui.removeSelectedCategoryClass(categoryCard);
         ui.removePulseCategoryContainer();
@@ -57,31 +62,80 @@ function additionalGameOptions() {
   return selectedGameOptions;
 }
 
-//play game
 document.getElementById('start-game-btn').addEventListener('click', () => {
-  playGame();
+  checkIfUserSelectedCategory();
 });
 
-function playGame() {
+function checkIfUserSelectedCategory() {
   const gameOptions = additionalGameOptions();
 
   if (!gameOptions.category) {
     ui.displayOptionsError('block', 'You must select a category');
     ui.pulseCategoryContainer();
   } else {
-    isGamePlaying = true;
-    getQuestions(gameOptions).then((data) => {
+    startGame();
+  }
+}
+
+function startGame() {
+  isGamePlaying = true;
+  fetchSessionToken().then((data) => {
+    const questionsUrl = `https://opentdb.com/api.php?amount=${selectedGameOptions.amountOfQuestions}&category=${selectedGameOptions.category}&difficulty=${selectedGameOptions.difficulty}&type=${selectedGameOptions.questionsOptions}&encode=base64&token=${data.token}`;
+
+    useApi(questionsUrl).then((data) => {
       if (data.response_code === 1) {
         ui.displayOptionsError(
           'block',
           'No Results The API does not have enough questions for your query, try other options'
         );
       } else {
-        ui.displayOptionsError('none');
-        ui.removeGameOptionsUi();
-        ui.createQuestion(data, currentQuestion);
+        playGame(data);
       }
     });
-    ui.removePulseCategoryContainer();
+  });
+  ui.removePulseCategoryContainer();
+}
+
+function playGame(data) {
+  if (currentQuestion === 0) {
+    initialQuestion(data);
   }
+  document.getElementById('next-question-btn').addEventListener('click', () => {
+    nextQuestion(data);
+  });
+}
+
+function initialQuestion(data) {
+  ui.removeGameOptionsUi();
+  ui.displayOptionsError('none');
+  ui.displayQuestion(data, 0);
+  initQuestionTimer();
+  selectedGameOptions.questionsOptions === 'multiple'
+    ? ui.createMultipleChoiceAnswers(data, 0)
+    : ui.createTrueFalseAnswers(data, currentQuestion);
+}
+
+function nextQuestion(data) {
+  currentQuestion++;
+  countdownTime = 5;
+  clearInterval(countdownTimer);
+  initQuestionTimer();
+  ui.removeCountdownAnimation();
+  ui.displayQuestion(data, currentQuestion);
+}
+
+function initQuestionTimer() {
+  const countdownNumber = document.getElementById('countdown-number');
+  countdownNumber.innerText = countdownTime;
+
+  countdownTimer = setInterval(() => {
+    if (countdownTime <= 0) {
+      clearInterval(countdownTimer);
+    } else {
+      --countdownTime;
+      ui.addCountdownAnimation();
+    }
+
+    countdownNumber.innerText = countdownTime;
+  }, 1000);
 }
