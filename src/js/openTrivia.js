d43e226000849737b65b029d6f223edc31580cea
Christian Predoianu <christianpredoianu@yahoo.com>
2022-09-13 16:48:06 +0200

    added fetch sessionToken

--- src/js/openTrivia.js
+++ src/js/openTrivia.js
@@ -1,25 +1,23 @@
 import { displayFetchError } from './ui';
 
-export async function getCategories() {
-  const url = 'https://opentdb.com/api_category.php';
-
+export async function useApi(url) {
   try {
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
+
     return data;
   } catch (error) {
     displayFetchError(error);
   }
 }
 
-export async function getQuestions(gameOptions) {
-  const url = `https://opentdb.com/api.php?amount=${gameOptions.amountOfQuestions}&category=${gameOptions.category}&difficulty=${gameOptions.difficulty}&type=${gameOptions.questionsOptions}&encode=base64`;
+export async function fetchSessionToken() {
+  const url = 'https://opentdb.com/api_token.php?command=request';
 
   try {
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data);
+
     return data;
   } catch (error) {
     displayFetchError(error);
